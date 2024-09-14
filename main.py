845924b5e9cd4598140548f4476da17dfca1e7cc
vedanshipanchal <vedanshipanchal@gmail.com>
2024-09-14 17:28:19 +0100

    And or

---
+++ main.py
@@ -0,0 +1,18 @@
+# part
+a = 10
+b = 0
+c = 5
+
+if a and b and c:
+    print("All the numbers have true")
+else:
+    print("Atleast one number is false")
+#2 part
+a = -10
+b = -20
+c = 0
+
+if a>0 or b>0:
+    print("either of the number is > 0")
+else:
+    print("no number is > 0")
